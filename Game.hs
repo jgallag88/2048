46e9423dc350e9701b705cae6bd96e393174bfc2
John Gallagher <jgallag88@gmail.com>
2014-03-16 20:04:12 -0400

    Initial commit
    Clone of the currently popular 2048 game. This contains the necessary
    types, along with a pretty printer.

---
+++ Game.hs
@@ -0,0 +1,82 @@
+--Version of 2048 game
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Game where
+
+import Data.Array as Ar
+import Text.Printf
+
+data Board = Board (Array Row (Array Col Square))
+             deriving (Show)
+
+--Type for indices of the board
+data Index = I0 | I1 | I2 | I3
+            deriving (Show, Eq, Ord, Bounded, Enum, Ix)
+    
+type Row = Index
+type Col = Index
+
+data Square = Full Block | Empty
+              deriving (Show)
+
+data Block = B2
+           | B4
+           | B8
+           | B16
+           | B32
+           | B64
+           | B128
+           | B256
+           | B512
+           | B1024
+           | B2048
+           deriving (Show, Eq, Ord, Enum)
+
+emptyBoard :: Board
+emptyBoard = Board $ array (minBound, maxBound) $ map (,mkRow) [minBound .. maxBound]
+    where mkRow = array (minBound, maxBound) $ map (,Empty) [minBound .. maxBound]
+
+getSquare :: Board -> (Row, Col) -> Square
+getSquare (Board board) (row, col) =
+    board ! row ! col 
+
+setSquare :: Board -> (Row, Col) -> Square -> Board
+setSquare (Board board) (row, col) sqr =
+    Board $ board // [(row, newRow)]
+    where newRow = board ! row // [(col, sqr)]
+
+--Pretty printer
+showBoard :: Board -> String
+showBoard board =  
+    foldl mkRow borderRow indexRange   
+    where 
+       mkRow acc row = acc ++ 
+                       concat (replicate botBlankLns blankLine) ++ 
+                       textLine ++ "\n" ++
+                       concat (replicate topBlankLns blankLine) ++ 
+                       borderRow
+            where textLine = foldl mkBox "|" indexRange
+                  mkBox acc' col = acc' ++
+                                   (padCenter width $ showSquare $ getSquare board (row, col))
+                                   ++ "|"
+                  blankLine = "|" ++ 
+                              (concat $ replicate rows (replicate width ' ' ++ "|")) ++
+                              "\n"
+                  botBlankLns = (height - 1) `div` 2
+                  topBlankLns = height - botBlankLns - 1
+       borderRow = replicate ((width+1)*rows + 1) '-' ++ "\n"
+       indexRange = [minBound .. maxBound] :: [Index]
+       rows = length indexRange
+       height = 3
+       width = 8
+
+padCenter :: Int -> String -> String
+padCenter len str = left ++ str ++ right
+    where
+       left = replicate ((len - (length str)) `div` 2) ' '
+       right = replicate (len - length str - length left) ' '
+
+showSquare :: Square -> String
+showSquare Empty = ""
+showSquare (Full b) = tail . show $ b
